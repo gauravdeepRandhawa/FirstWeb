1544d95daab4c3ac0ba2dd670a79e9bf37bed20e
gauravdeep <er.gauravdeep11@gmail.com>
2020-07-24 13:21:30 +1200

    First Commit

---
+++ services/computer-repairs.php
@@ -0,0 +1,400 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+        <base href="../">
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Repair</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">							
+			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body class="blog-page">
+		<?php include_once "../common/menu.php"?>
+        <!-- #header -->
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Computer Repairs
+							</h1>	
+							<p class="text-white link-nav"><a href="../index.php">Home </a>  <span class="lnr lnr-arrow-right"></span><a>Service </a> <span class="lnr lnr-arrow-right"></span> <a href="computer-repairs.php"> Computer Repairs</a></p>
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->					  
+			
+			<!-- Start post-content Area -->
+			<section class="post-content-area single-post-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 posts-list">
+							<div class="single-post row">
+								<div class="col-lg-12">
+									<div class="feature-img">
+										<img class="img-fluid" src="img/blog/feature-img1.jpg" alt="">
+									</div>									
+								</div>
+
+								<div class="col-lg-12 col-md-12">
+									<a class="posts-title" ><h3>
+                                            Premium Service at an Affordable Cost
+                                        </h3></a>
+									<p class="excert">
+                                        We receive most business through word of mouth and have many happy return customers.
+									</p>
+									<p>
+                                        You pay only for the support you receive. For a robust IT environment, with friendly support, you can rely on just a phone call away.
+									</p>
+									<p>
+                                        Our staff are dedicated to helping Small to Medium sized businesses in South Africa. If you are looking for reliable, efficient and friendly maintenance, management and support for your business technology or infrastructure give us a call!
+									</p>
+								</div>
+								<div class="col-lg-12">
+
+									<div class="row mt-30 mb-30">
+										<div class="col-6">
+											<img class="img-fluid" src="img/blog/post-img1.jpg" alt="">
+										</div>
+										<div class="col-6">
+											<img class="img-fluid" src="img/blog/post-img2.jpg" alt="">
+										</div>	
+
+									</div>
+								</div>
+							</div>
+
+							<div class="comments-area">
+								<h4>05 Comments</h4>
+								<div class="comment-list">
+                                    <div class="single-comment justify-content-between d-flex">
+                                        <div class="user justify-content-between d-flex">
+                                            <div class="thumb">
+                                                <img src="img/blog/c1.jpg" alt="">
+                                            </div>
+                                            <div class="desc">
+                                                <h5><a href="#">Emilly Blunt</a></h5>
+                                                <p class="date">December 4, 2017 at 3:12 pm </p>
+                                                <p class="comment">
+                                                    Never say goodbye till the end comes!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="reply-btn">
+                                               <a href="" class="btn-reply text-uppercase">reply</a> 
+                                        </div>
+                                    </div>
+                                </div>	
+								<div class="comment-list left-padding">
+                                    <div class="single-comment justify-content-between d-flex">
+                                        <div class="user justify-content-between d-flex">
+                                            <div class="thumb">
+                                                <img src="img/blog/c2.jpg" alt="">
+                                            </div>
+                                            <div class="desc">
+                                                <h5><a href="#">Elsie Cunningham</a></h5>
+                                                <p class="date">December 4, 2017 at 3:12 pm </p>
+                                                <p class="comment">
+                                                    Never say goodbye till the end comes!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="reply-btn">
+                                               <a href="" class="btn-reply text-uppercase">reply</a> 
+                                        </div>
+                                    </div>
+                                </div>	
+								<div class="comment-list left-padding">
+                                    <div class="single-comment justify-content-between d-flex">
+                                        <div class="user justify-content-between d-flex">
+                                            <div class="thumb">
+                                                <img src="img/blog/c3.jpg" alt="">
+                                            </div>
+                                            <div class="desc">
+                                                <h5><a href="#">Annie Stephens</a></h5>
+                                                <p class="date">December 4, 2017 at 3:12 pm </p>
+                                                <p class="comment">
+                                                    Never say goodbye till the end comes!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="reply-btn">
+                                               <a href="" class="btn-reply text-uppercase">reply</a> 
+                                        </div>
+                                    </div>
+                                </div>	
+								<div class="comment-list">
+                                    <div class="single-comment justify-content-between d-flex">
+                                        <div class="user justify-content-between d-flex">
+                                            <div class="thumb">
+                                                <img src="img/blog/c4.jpg" alt="">
+                                            </div>
+                                            <div class="desc">
+                                                <h5><a href="#">Maria Luna</a></h5>
+                                                <p class="date">December 4, 2017 at 3:12 pm </p>
+                                                <p class="comment">
+                                                    Never say goodbye till the end comes!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="reply-btn">
+                                               <a href="" class="btn-reply text-uppercase">reply</a> 
+                                        </div>
+                                    </div>
+                                </div>	
+								<div class="comment-list">
+                                    <div class="single-comment justify-content-between d-flex">
+                                        <div class="user justify-content-between d-flex">
+                                            <div class="thumb">
+                                                <img src="img/blog/c5.jpg" alt="">
+                                            </div>
+                                            <div class="desc">
+                                                <h5><a href="#">Ina Hayes</a></h5>
+                                                <p class="date">December 4, 2017 at 3:12 pm </p>
+                                                <p class="comment">
+                                                    Never say goodbye till the end comes!
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="reply-btn">
+                                               <a href="" class="btn-reply text-uppercase">reply</a> 
+                                        </div>
+                                    </div>
+                                </div>	                                             				
+							</div>
+							<div class="comment-form">
+								<h4>Leave a Comment</h4>
+								<form>
+									<div class="form-group form-inline">
+									  <div class="form-group col-lg-6 col-md-12 name">
+									    <input type="text" class="form-control" id="name" placeholder="Enter Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Name'">
+									  </div>
+									  <div class="form-group col-lg-6 col-md-12 email">
+									    <input type="email" class="form-control" id="email" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'">
+									  </div>										
+									</div>
+									<div class="form-group">
+										<input type="text" class="form-control" id="subject" placeholder="Subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Subject'">
+									</div>
+									<div class="form-group">
+										<textarea class="form-control mb-10" rows="5" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required=""></textarea>
+									</div>
+									<a href="#" class="primary-btn text-uppercase">Post Comment</a>	
+								</form>
+							</div>
+						</div>
+						<div class="col-lg-4 sidebar-widgets">
+							<div class="widget-wrap">
+								<div class="single-sidebar-widget user-info-widget">
+									<img src="img/blog/user-info.png" alt="">
+									<a href="#"><h4>Charlie Barber</h4></a>
+									<p>
+                                        We don't believe in charging for something that is not fixed. Our No Fix No Fee Guarantee means we will not charge you if we do not fix your device
+									</p>
+								</div>
+
+								<div class="single-sidebar-widget post-category-widget">
+									<h4 class="category-title">Popular Fixes</h4>
+									<ul class="cat-list">
+										<li>
+											<a  class="d-flex justify-content-between">
+												<p>Computer and Laptop Upgrades</p>
+
+											</a>
+										</li>
+										<li>
+											<a class="d-flex justify-content-between">
+												<p>Spyware and virus infections</p>
+											</a>
+										</li>
+										<li>
+											<a class="d-flex justify-content-between">
+												<p>Slow performance</p>
+											</a>
+										</li>
+										<li>
+											<a class="d-flex justify-content-between">
+												<p>Keyboard, touchpad and screen replacements</p>
+											</a>
+										</li>
+										<li>
+											<a class="d-flex justify-content-between">
+												<p>Setting up, configuring and installing applications</p>
+											</a>
+										</li>
+										<li>
+											<a  class="d-flex justify-content-between">
+												<p>Migration of data and software to another laptop</p>
+
+											</a>
+										</li>
+										<li>
+											<a class="d-flex justify-content-between">
+												<p>Replacement of hard drive and installing operating system
+                                                </p>
+											</a>
+										</li>															
+									</ul>
+								</div>	
+								<div class="single-sidebar-widget newsletter-widget">
+									<h4 class="newsletter-title">Newsletter</h4>
+									<p>
+										Here, I focus on a range of items and features that we use in life without
+										giving them a second thought.
+									</p>
+									<div class="form-group d-flex flex-row">
+									   <div class="col-autos">
+									      <div class="input-group">
+									        <div class="input-group-prepend">
+									          <div class="input-group-text"><i class="fa fa-envelope" aria-hidden="true"></i>
+											</div>
+									        </div>
+									        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email'" >
+									      </div>
+									    </div>
+									    <a href="#" class="bbtns">Subcribe</a>
+									</div>	
+									<p class="text-bottom">
+										You can unsubscribe at any time
+									</p>								
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End post-content Area -->
+			
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>About Agency</h6>
+								<p>
+									The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point 
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Navigation Links</h6>
+								<div class="row">
+									<div class="col">
+										<ul>
+											<li><a href="#">Home</a></li>
+											<li><a href="#">Feature</a></li>
+											<li><a href="#">Services</a></li>
+											<li><a href="#">Portfolio</a></li>
+										</ul>
+									</div>
+									<div class="col">
+										<ul>
+											<li><a href="#">Team</a></li>
+											<li><a href="#">Pricing</a></li>
+											<li><a href="#">Blog</a></li>
+											<li><a href="#">Contact</a></li>
+										</ul>
+									</div>										
+								</div>							
+							</div>
+						</div>							
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Newsletter</h6>
+								<p>
+									For business professionals caught between high OEM price and mediocre print and graphic output.									
+								</p>								
+								<div id="mc_embed_signup">
+									<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
+										<div class="input-group d-flex flex-row">
+											<input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+											<div style="position: absolute; left: -5000px;">
+												<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+											</div>
+											<button class="btn bb-btn"><span class="lnr lnr-location"></span></button>		
+										</div>									
+										<div class="mt-10 info"></div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3  col-md-6 col-sm-6">
+							<div class="single-footer-widget mail-chimp">
+								<h6 class="mb-20">InstaFeed</h6>
+								<ul class="instafeed d-flex flex-wrap">
+									<li><img src="img/i1.jpg" alt=""></li>
+									<li><img src="img/i2.jpg" alt=""></li>
+									<li><img src="img/i3.jpg" alt=""></li>
+									<li><img src="img/i4.jpg" alt=""></li>
+									<li><img src="img/i5.jpg" alt=""></li>
+									<li><img src="img/i6.jpg" alt=""></li>
+									<li><img src="img/i7.jpg" alt=""></li>
+									<li><img src="img/i8.jpg" alt=""></li>
+								</ul>
+							</div>
+						</div>						
+					</div>
+
+					<div class="row footer-bottom d-flex justify-content-between align-items-center">
+						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+</p>
+						<div class="col-lg-4 col-sm-12 footer-social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->	
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/mn-accordion.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+			<script src="js/owl.carousel.min.js"></script>						
+			<script src="js/jquery.nice-select.min.js"></script>	
+    		<script src="js/jquery.circlechart.js"></script>								
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
